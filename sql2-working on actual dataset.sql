167c554b8fda52e25af76761adc1ce98d0961c5e
Kate Han <92063672+katieeehan20@users.noreply.github.com>
2022-07-02 23:27:59 -0400

    Add files via upload

--- sql2-working on actual dataset.sql
+++ sql2-working on actual dataset.sql
@@ -139,3 +139,146 @@ order by first_name, last_name asc;
 
 select*from employees
 order by hire_date desc;
+
+select first_name
+from employees;
+
+select first_name
+from employees
+group by first_name;
+
+select distinct first_name
+from employees;
+
+select count(first_name)
+from employees
+group by (first_name);
+
+select first_name, count(first_name)
+from employees
+group by first_name
+order by first_name DESC;
+
+select hire_date
+from employees
+group by hire_date
+order by hire_date DESC;
+
+select salary 
+from salaries
+group by salary
+order by salary DESC;
+
+select first_name, count(first_name) as number_of_times_the_name_appears
+from employees
+group by first_name
+order by first_name DESC;
+
+select*from salaries;
+
+select salary, count(emp_no) as emp_with_same_salary
+from salaries
+where salary>80000
+group by salary
+order by salary DESC;
+
+select salary
+from salaries
+having salary>100000;
+
+SELECT 
+    first_name, COUNT(first_name) AS names_count
+FROM
+    employees
+GROUP BY first_name
+having count(first_name) >250
+order by first_name;
+
+select emp_no, avg(salary)
+from salaries
+group by emp_no
+having avg(salary)>120000;
+
+select *, avg(salary)
+from salaries
+where salary>120000
+group by emp_no
+order by emp_no;
+
+select *, avg(salary)
+from salaries
+group by emp_no
+having avg(salary)>120000;
+
+select first_name, count(first_name) as names_count
+from employees
+where hire_date > '1999-01-01'
+group by first_name
+having count(first_name)<200
+order by count(first_name) Desc;
+
+select*from dept_emp;
+
+select emp_no 
+from dept_emp
+where from_date>'2000-01-01'
+group by emp_no
+having count(from_date)>1
+order by emp_no;
+select*from salaries
+having salary>150000
+order by salary desc
+limit 10;
+
+select *from employees;
+
+select first_name, last_name, count(last_name)
+from employees
+where hire_date>'2000-01-01'
+group by last_name
+having count(first_name)<100
+order by last_name
+limit 100;
+
+select *
+from dept_emp
+limit 100;
+
+select *
+from titles
+limit 10;
+
+alter table titles
+drop foreign key titles_ibfk_1;
+
+alter table titles
+drop primary key;
+
+insert into titles (emp_no, title, from_date)
+values (999903, 'Senior Engineer', '1997-10-01');
+
+select*from titles
+order by emp_no desc;
+
+select*from dept_emp
+order by emp_no desc;
+
+alter table dept_emp
+drop foreign key dept_emp_ibfk_1;
+
+alter table dept_emp
+drop foreign key  dept_emp_ibfk_2;
+
+insert into dept_emp 
+values (999903, 'd005', '1997-10-01',"9999-01-01");
+
+select*from dept_emp
+order by emp_no desc;
+
+select*from departments;
+
+insert into departments 
+values ('d010', 'Business Analysis');
+
+select*from departments
+order by dept_name asc;
