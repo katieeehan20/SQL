d7cadcb35cc0222882d189b36150a83d2ece7bcf
Kate Han <92063672+katieeehan20@users.noreply.github.com>
2022-07-04 22:07:31 -0400

    Add files via upload

--- sql2-working on actual dataset.sql
+++ sql2-working on actual dataset.sql
@@ -492,3 +492,46 @@ select
     
 select
 	coalesce ('First', null, 'Third') as coalesce_Test;
+    
+alter table departments_dup
+drop column dept_manager;
+
+alter table departments_dup
+change column dept_no dept_no char(4) null;
+
+alter table departments_dup
+change column dept_name dept_name varchar(40) default null;
+
+delete from departments_dup
+where dept_no='d002';
+
+select*from departments_dup;
+
+delete from departments_dup 
+where dept_no= 'd012';
+delete from departments_dup
+where dept_no='d013';
+
+drop table if exists dept_manager_dup;
+
+create table dept_manager_dup
+(
+	emp_no int(11) not null,
+    dept_no char(4) null,
+    from_date date not null,
+    to_date date null);
+
+insert into dept_manager_dup (emp_no, from_date)
+values (999904, '2017-01-01'),
+		(999905, '2017-01-01'),
+		(999906, '2017-01-01'),
+		(999907, '2017-01-01');
+
+select*from departments_dup;
+
+delete from departments_dup
+where dept_no='d002';
+
+insert into departments_dup
+	(dept_no, dept_name)
+values ('d001', 'public relations');
